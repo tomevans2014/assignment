a6cced99d5e359474f18be29f98afc750c085008
tom_evans97@outlook.com <41452@PC004149.coll.lr.local>
2014-09-09 12:57:37 +0100

    second commitment whoop whoop

---
+++ assignment/second.py
@@ -0,0 +1,11 @@
+#Tom Evans
+#09/09/14
+#hello world
+
+
+print("hello world")
+first_name = input("please enter first name:")
+#asks computor to go to store input as 'first_name'
+print(first_name)
+print("hey there {0}".format(first_name))
+#asks computor to go to storage location 'first_name'
